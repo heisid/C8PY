90e0823e8f458cd443ec379cce860269b854bd9e
heisid <weird_alien@protonmail.com>
2022-03-05 12:22:04 +0700

    formatting

--- cpu.py
+++ cpu.py
@@ -1,47 +1,56 @@
 class CPU:
     def __init__(self):
-        # Program counter
-        # The first CHIP-8 interpreter (on the COSMAC VIP computer) 
-        # was also located in RAM, from address 000 to 1FF.
-        # It would expect a CHIP-8 program to be loaded into memory after it, 
-        # starting at address 200 (512 in decimal)
+        '''
+        CPU initialization of
+        program counter, index register, stack memory,
+        timers, and general purpose registers
+        '''
+        '''
+        The first CHIP-8 interpreter (on the COSMAC VIP computer)
+        was also located in RAM, from address 000 to 1FF.
+        It would expect a CHIP-8 program to be loaded into memory after it,
+        starting at address 200 (512 in decimal)
+        '''
         self.pc = 0x200
 
-        # Index register
+        ''' Index register '''
         self.i = 0x0
 
-        # Stack
+        ''' Stack '''
         self.stack = list()
 
-        # Timers
+        ''' Timers '''
         self.delay_timer = 0
         self.sound_timer = 0
 
-        # 16 of 8-bit General purpose registers
+        ''' 16 of 8-bit General purpose registers '''
         self.v = [0x0] * 16
 
+
     def load_font(self, ram):
-        # The CHIP-8 emulator should have a built-in font
-        # Each font character should be 4 pixels wide by 5 pixels tall.
-        # For some reason, it’s become popular to put it at 050–09F
+        '''
+        The CHIP-8 emulator should have a built-in font
+        Each font character should be 4 pixels wide by 5 pixels tall.
+        For some reason, it’s become popular to put it at 050–09F
+        '''
         FONT = [
-                0xF0, 0x90, 0x90, 0x90, 0xF0, # 0
-                0x20, 0x60, 0x20, 0x20, 0x70, # 1
-                0xF0, 0x10, 0xF0, 0x80, 0xF0, # 2
-                0xF0, 0x10, 0xF0, 0x10, 0xF0, # 3
-                0x90, 0x90, 0xF0, 0x10, 0x10, # 4
-                0xF0, 0x80, 0xF0, 0x10, 0xF0, # 5
-                0xF0, 0x80, 0xF0, 0x90, 0xF0, # 6
-                0xF0, 0x10, 0x20, 0x40, 0x40, # 7
-                0xF0, 0x90, 0xF0, 0x90, 0xF0, # 8
-                0xF0, 0x90, 0xF0, 0x10, 0xF0, # 9
-                0xF0, 0x90, 0xF0, 0x90, 0x90, # A
-                0xE0, 0x90, 0xE0, 0x90, 0xE0, # B
-                0xF0, 0x80, 0x80, 0x80, 0xF0, # C
-                0xE0, 0x90, 0x90, 0x90, 0xE0, # D
-                0xF0, 0x80, 0xF0, 0x80, 0xF0, # E
-                0xF0, 0x80, 0xF0, 0x80, 0x80  # F
-                ]
+            0xF0, 0x90, 0x90, 0x90, 0xF0,  # 0
+            0x20, 0x60, 0x20, 0x20, 0x70,  # 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,  # 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,  # 3
+            0x90, 0x90, 0xF0, 0x10, 0x10,  # 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,  # 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,  # 6
+            0xF0, 0x10, 0x20, 0x40, 0x40,  # 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,  # 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0,  # 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90,  # A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,  # B
+            0xF0, 0x80, 0x80, 0x80, 0xF0,  # C
+            0xE0, 0x90, 0x90, 0x90, 0xE0,  # D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,  # E
+            0xF0, 0x80, 0xF0, 0x80, 0x80,  # F
+        ]
 
         start_font_addr = 0x050
         ram.bulk_write(start_font_addr, FONT)
--- ram.py
+++ ram.py
@@ -1,62 +1,57 @@
 import sys
 
+
 class RAM:
     def __init__(self):
-        # The memory should be 4 kB (4 kilobytes, ie. 4096 bytes) large
-        self.data = [0] * 4096
+        '''
+        RAM Initialization, set all to zeros
+        The memory should be 4 kB (4 kilobytes, ie. 4096 bytes) large
+        '''
+        self.data = [0x00] * 4096
+
 
     def address_exception(func):
-        # Exception handler for out of range memory
-        # address must be the first argument
+        '''
+        Exception handler for out of range memory
+        address must be the first argument
+        '''
         def wrapper(*args, **kwargs):
             address = args[1]
             try:
-                if 0x000 <= address <= 0xfff:
+                if 0x000 <= address <= 0xFFF:
                     return func(*args, **kwargs)
                 else:
                     raise IndexError()
             except IndexError:
-                print(f'Invalid memory address: {hex(address)}', file=sys.stderr)
+                print(f"Invalid memory address: {hex(address)}", file=sys.stderr)
                 sys.exit(1)
+
         return wrapper
+
 
     @address_exception
     def __getitem__(self, address):
         return self.data[address]
 
+
     @address_exception
     def __setitem__(self, address, value):
-        # Limit only 8-bit per address slot
-        self.data[address] = 0xff & value
+        ''' Limit only 8-bit per address slot '''
+        self.data[address] = 0xFF & value
+
 
     def bulk_write(self, start_addr, data):
-        # Write many bytes at once in contiguous memory space
+        ''' Write many bytes at once in contiguous memory space '''
         for idx, data_byte in enumerate(data):
             self.__setitem__(start_addr + idx, data_byte)
+
 
     def __len__(self):
         return len(self.data)
 
+
     def __str__(self):
-        printed_str = ''
+        printed_str = ""
         for address, value in enumerate(self.data):
-            printed_str += f'0x{address:03x}: 0x{value:02x}\n'
+            printed_str += f"0x{address:03x}: 0x{value:02x}\n"
         return printed_str
-
-
-# Testing purpose
-if __name__ == '__main__':
-    # Initialization test
-    ram = RAM()
-    print(len(ram))
-
-    # Write test
-    ram[0x100] = 0xfff # should return 0xff on read
-
-    # Read test
-    print(hex(ram[0x100]))
-    #ram[0xfff + 1] # should raise exception
-    #ram[0xfff + 1] = 100
-
-    ram[0xfff] = 0x42
-    print(ram)
