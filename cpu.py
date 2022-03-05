c2c28dc01e03530759edbaa80995aec9f3bde9be
heisid <weird_alien@protonmail.com>
2022-03-05 14:24:04 +0700

    first opcode

--- cpu.py
+++ cpu.py
@@ -57,6 +57,62 @@ class CPU:
 
 
     def fetch(self, ram):
+        '''
+        Read the instruction that PC is currently pointing at from memory. 
+        An instruction is two bytes.
+        '''
         opcode = (ram[self.pc] << 8) | ram[self.pc + 1]
         self.pc += 2
         return opcode
+
+
+    def execute(self, opcode):
+        '''
+        Choosing function to execute opcode based on its class (first two bits)
+        '''
+        opcode_type = 0xf000 & opcode
+        OPCODE_TYPE_MAP = {
+                0x0000: self.op_0,
+                0x1000: self.op_1,
+                0x2000: self.op_2,
+                0X3000: self.op_3,
+                0x4000: self.op_4,
+                0x5000: self.op_5,
+                0x6000: self.op_6,
+                0x7000: self.op_7,
+                0x8000: self.op_8,
+                0x9000: self.op_9,
+                0xa000: self.op_a,
+                0xb000: self.op_b,
+                0xc000: self.op_c,
+                0xd000: self.op_d,
+                0xe000: self.op_e,
+                0xf000: self.op_f
+                }
+        
+        '''
+        Execute opcode with argument of the last 12 bits of the opcode
+        because we don't need the first 4 bits anyway.
+        op_x will return false if the code is invalid, true otherwise.
+        so, check the validity, and raise exception if it's not.
+        '''
+        exe_status = OPCODE_TYPE_MAP[opcode_type](0x0fff & opcode)
+        
+        if not exe_status:
+            print(f"Invalid opcode: {opcode} at {self.pc - 2}")
+            sys.exit(1)
+
+
+    def op_0(self, arg):
+        if arg == 0x0e0:
+            ''' 0x00e0: CLS: Clear screen '''
+            pass # IMPLEMENT LATER
+
+        elif arg = 0x0ee:
+            ''' 0x00ee: RET: Return from subroutine '''
+            self.pc = self.stack.pop()
+
+        else:
+            return False
+
+        return True
